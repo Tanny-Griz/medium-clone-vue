5388e2b555172a53d6bf3fbca7e5acbfe82ec5bd
Tanya Kovalenko <tanya.griz89@gmail.com>
2021-02-03 18:53:32 +0200

    added article page

--- src/store/index.js
+++ src/store/index.js
@@ -4,25 +4,19 @@ import Vuex from 'vuex'
 import auth from '@/store/modules/auth'
 import feed from '@/store/modules/feed'
 import tags from '@/store/modules/tags'
-// import article from '@/store/modules/article'
+import article from '@/store/modules/article'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   modules: {
     auth,
     feed,
     tags,
-    // article f47aae4e-797e-4c82-b058-b604646da633
-
+    article
   }
 })
  
